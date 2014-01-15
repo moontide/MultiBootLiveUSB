f113a51e18e95f9c625bd0a30499ed9d79748cea
LiuYan 刘研 <lovetide@qq.com>
2014-01-15 18:31:51 +0800

    所有都 com32 文件不再拷贝到 U 盘，只拷贝指定都、必需都文件；efi32/efi64 的文件比 bios 都多了几个，单独列出

--- boot/syslinux/copy-c32.sh
+++ boot/syslinux/copy-c32.sh
@@ -1,3 +1,4 @@
+cur_dir=$PWD
 dst_dir=$(dirname "$0")
 script=$(basename "$0")
 type="$1"
@@ -7,8 +8,10 @@ dst_dir="$3"
 echo "type	= $type"
 echo "src_dir	= $src_dir"
 echo "dst_dir	= $dst_dir"
-
-return
+# 如果目的地为“.”（当前目录），则需要改为绝对路径
+if [[ "$dst_dir" == "." ]]; then
+	dst_dir="$cur_dir"
+fi
 
 usage ()
 {
@@ -22,10 +25,21 @@ then
 	exit 1
 fi
 
-cd "$src_dir"
-find -name "*.c32" -or -name "*.e32" -or -name "*.e64" -exec cp --preserve -f --parents {} "$dst_dir" \;
+shopt -s nocasematch
+case "$type" in
+	efi32)
+		extra_files="ldlinux.e32 syslinux.efi"
+		;;
+	efi64)
+		extra_files="ldlinux.e64 syslinux.efi"
+		;;
+esac
 
-for f in chain.c32 gfxboot.c32 hdt.c32 ifcpu64.c32 libcom32.c32 libgpl.c32 liblua.c32 libmenu.c32 libutil.c32 linux.c32 menu.c32 reboot.c32 vesamenu.c32 memdisk
+cd "$src_dir"
+#find $type/com32 -name "*.c32" -or -name "*.e32" -or -name "*.e64" -exec cp --preserve -f --parents {} "$dst_dir" \;
+
+for f in chain.c32 gfxboot.c32 hdt.c32 ifcpu64.c32 libcom32.c32 libgpl.c32 liblua.c32 libmenu.c32 libutil.c32 linux.c32 menu.c32 reboot.c32 vesamenu.c32 memdisk $extra_files
 do
-	find "$src_dir/$type" -type f -name $f -exec cp --preserve -f {} "$dst_dir" \;
+	find "$src_dir/$type" -type f -name $f -print -exec cp --preserve -f {} "$dst_dir" \;
 done
+cd "$cur_dir"
